b36cdc9d00199fc39720b0e1a0f69581dfb5bc3f
munjasolanke <54603212+munjasolanke@users.noreply.github.com>
2019-09-10 21:02:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many years of professional experience do you have");
+            string input = Console.ReadLine();
+            if(int.Parse(input) != 0)
+            {
+                for (int i = 1; i <= int.Parse(input); i++)
+                {
+                    Console.WriteLine("you have {0} number of year experience", i);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("you have zero experience");
+            }
+
+            
+
+        }
+    }
+}
